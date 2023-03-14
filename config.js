d04b8801aa4c0293d80609e7cceda8abf3914368
onlypeng <15733181556@163.com>
2023-03-14 17:01:16 +0800

    更新多用户隔离同步

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  test1: { // key 用户名
+  test1: { // key 用户1
     userName: '', // 备用用户名，默认不填为key(test1),暂时没用
     connectPasword: 'dddd', // 连接密码，若在外网，务必增加密码复杂度,密码唯一，根据链接的密码自动分组用户
     maxSsnapshotNum: 10, // 最大备份快照数
     'list.addMusicLocationType': 'top', // 添加歌曲到我的列表时的方式 top | bottom
   },
-  test2: { // key 用户名
+  test2: { // key 用户2
     userName: '', // 备用用户名，默认不填为key(test2),暂时没用
     connectPasword: 'ssss', // 连接密码，若在外网，务必增加密码复杂度,密码唯一，根据链接的密码自动分组用户
     maxSsnapshotNum: 10, // 最大备份快照数
